29de13297bd20923524bdc1b024be1e8dd1ae1f7
Dmitrijs Asanovs <dmitrijsasanovs@Dmitrijss-MacBook-Air.local>
2019-04-19 22:27:39 +0300

    right and middle block alignment

--- app/component/Header/Header.component.js
+++ app/component/Header/Header.component.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './Header.style';
 
 const PDP = 'pdp';
+const CATEGORY = 'category';
+const CUSTOMER_ACCOUNT = 'customer_account';
+const HOME_PAGE = 'home';
 
 class Header extends Component {
     constructor(props) {
@@ -11,6 +14,23 @@ class Header extends Component {
             [PDP]: {
                 navigation: true,
                 title: true,
+                minicart: true
+            },
+            [CATEGORY]: {
+                navigation: true,
+                search: true,
+                title: true,
+                minicart: true
+            },
+            [CUSTOMER_ACCOUNT]: {
+                navigation: true,
+                title: true
+            },
+            [HOME_PAGE]: {
+                navigation: true,
+                search: true,
+                title: true,
+                account: true,
                 minicart: true
             }
         };
@@ -23,16 +43,33 @@ class Header extends Component {
             <>
                 {/* <button block="Header" elem="NavigationClose" />
                 <button block="Header" elem="NavigationBack" /> */}
-                <span>B</span>
-                <span>C</span>
+                <span block="Header" elem="Icon">B</span>
+                <span block="Header" elem="Icon">C</span>
+            </>
+        );
+    }
+
+    renderSearchButton() {
+        return (
+            <>
+                <span block="Header" elem="Icon">S</span>
             </>
         );
     }
 
     renderTitle(title) {
+        // const { title } = this.props REDUX
         return (
             <>
-                <h1 block="Header" elem="Title">{ title }</h1>
+                <span block="Header" elem="Title">{ title }</span>
+            </>
+        );
+    }
+
+    renderAccountButton() {
+        return (
+            <>
+                <span block="Header" elem="Icon">A</span>
             </>
         );
     }
@@ -40,27 +77,32 @@ class Header extends Component {
     renderMinicartButton() {
         return (
             <>
-                <span block="Header" elem="MinicartButton">C</span>
+                <span block="Header" elem="Icon">2 C</span>
             </>
         );
     }
 
     renderHeaderState(state) {
+        // const { state } = this.props; ROUTER
         const {
             navigation,
             title,
-            minicart
+            minicart,
+            account,
+            search
         } = this.stateMap[state];
 
         return (
             <>
                 <div block="Header" elem="LeftSide">
                     { navigation && this.renderNavigationButton() }
+                    { search && this.renderSearchButton() }
                 </div>
                 <div block="Header" elem="Middle">
                     { title && this.renderTitle('Hello world') }
                 </div>
                 <div block="Header" elem="RightSide">
+                    { account && this.renderAccountButton() }
                     { minicart && this.renderMinicartButton() }
                 </div>
             </>
@@ -71,7 +113,7 @@ class Header extends Component {
         return (
             <header block="Header">
                 <div block="Header" elem="Wrapper">
-                    { this.renderHeaderState('pdp') }
+                    { this.renderHeaderState('home') }
                 </div>
             </header>
         );
